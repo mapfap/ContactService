bacaff303341237d7c633feeee8cda4cd1a7778a
mapfap <mapkabb@gmail.com>
2014-10-08 13:47:45 +0700

    Remove usued imports

--- src/contact/resource/ContactResource.java
+++ src/contact/resource/ContactResource.java
@@ -1,27 +1,19 @@
 package contact.resource;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.inject.Singleton;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import javax.xml.bind.JAXBElement;
 
 import contact.entity.Contact;
 import contact.entity.Contacts;
